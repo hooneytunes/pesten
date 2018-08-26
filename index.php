07d53d111511588ba15b298d93aac119c2bb5b07
hooneytunes <rob@familiedehoon.nl>
2018-08-26 13:36:26 +0200

    Logging is klaar.

--- index.php
+++ index.php
@@ -9,7 +9,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="css/normalize.css">
 	<link rel="stylesheet" type="text/css" href="css/skeleton.css">
-	<script src="js/pesten.js"></script> 
     </head>
     <body>
 	<div class="container">
@@ -18,5 +17,6 @@
 		<div class="eight columns">&nbsp;</div>
 	    </div>
 	</div>
+	<script src="js/pesten.js"></script> 
     </body>
 </html>
