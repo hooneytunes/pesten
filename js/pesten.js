677a4b5f21aeed28a313f18e037f25b9cb632682
hooneytunes <rob@familiedehoon.nl>
2018-08-27 21:56:51 +0200

    Kaartobject aangemaakt.

--- js/pesten.js
+++ js/pesten.js
@@ -1,5 +1,5 @@
 function clicked(tekst) {
-    var AjaxTP = new XMLHttpRequest();
+    let AjaxTP = new XMLHttpRequest();
     AjaxTP.onreadystatechange = function () {
 	if (this.readyState === 4 && this.status === 200) {
 	    document.getElementById('output').innerHTML = this.responseText;
@@ -8,3 +8,29 @@ function clicked(tekst) {
     AjaxTP.open("GET", "loggen.php?iTekst=" + tekst, true);
     AjaxTP.send();
 }
+
+function Kaart(kleur, waarde) {
+    let kaartKleur = null;
+    let kaartWaarde = null;
+
+    if (kleur < 1 || kleur > 4) {
+	throw new Error('Kleur niet toegestaan');
+    } else {
+	kaartKleur = kleur;
+    }
+
+    if (waarde < 1 || waarde > 13) {
+	throw new Error('Waarde niet toegestaan');
+    } else {
+	kaartWaarde = waarde;
+    }
+
+    this.getKaartKleur = function () {
+	return kaartKleur;
+    };
+
+    this.getKaartWaarde = function () {
+	return kaartWaarde;
+    };
+}
+
