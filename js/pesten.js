5c7f08e05591f48a4583f1eb4e27c10b62f698d9
hooneytunes <rob@familiedehoon.nl>
2018-08-27 22:04:47 +0200

    Kaartobject aangemaakt.

--- js/pesten.js
+++ js/pesten.js
@@ -10,8 +10,15 @@ function clicked(tekst) {
 }
 
 function Kaart(kleur, waarde) {
+    /*
+     * Zet de private variabelen op een startwaarde
+     */ 
     let kaartKleur = null;
     let kaartWaarde = null;
+    /*
+     * De kleur moet 1, 2, 3, of 4 zijn,
+     * anders wordt een foutmelding gegeven
+     */
 
     if (kleur < 1 || kleur > 4) {
 	throw new Error('Kleur niet toegestaan');
@@ -19,18 +26,28 @@ function Kaart(kleur, waarde) {
 	kaartKleur = kleur;
     }
 
+    /*
+     * De waarde varieert van 1 tot en met 13,
+     * anders wordt een foutmelding gegeven
+     */
+
     if (waarde < 1 || waarde > 13) {
 	throw new Error('Waarde niet toegestaan');
     } else {
 	kaartWaarde = waarde;
     }
 
+    /*
+     * Hier wordt de kaartkleur teruggegeven als waarde
+     */
     this.getKaartKleur = function () {
 	return kaartKleur;
     };
 
+    /*
+     * Hier wordt de kaartwaarde teruggegeven als waarde
+     */
     this.getKaartWaarde = function () {
 	return kaartWaarde;
     };
 }
-
