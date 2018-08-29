01714504d06ade2e56b9157c0ed177ba0ef03b55
hooneytunes <rob@familiedehoon.nl>
2018-08-29 22:29:40 +0200

    Kaartobject aangemaakt.

--- index.php
+++ index.php
@@ -10,13 +10,57 @@
 	<link rel="stylesheet" type="text/css" href="css/normalize.css">
 	<link rel="stylesheet" type="text/css" href="css/skeleton.css">
     </head>
-    <body>
+    <body onload="init();">
 	<div class="container">
 	    <div class="row">
-		<div class="three columns"><h3>Pesten</h3></div>
-		<div class="eight columns">&nbsp;</div>
+		<h3>Pesten</h3>
+	    </div>
+	    <div class="row">
+		<div class="one-half column">
+		    <table>
+			<caption id="player0_name">Rob</caption>
+			<tr><td>Klaveren Heer</td><td>Klaveren Heer</td><td>Klaveren Heer</td><td>Klaveren Heer</td><td>Klaveren Heer</td></tr>
+			<tr><td>Klaveren Heer</td><td>Klaveren Heer</td><td>Klaveren Heer</td><td>Klaveren Heer</td><td>Klaveren Heer</td></tr>
+			<tr><td>Klaveren Heer</td><td>Klaveren Heer</td><td>Klaveren Heer</td><td>Klaveren Heer</td><td>Klaveren Heer</td></tr>
+		    </table>
+		</div>
+		<div class="one-half column">
+		    <table>
+			<caption id="player1_name">Rob</caption>
+			<tr><td>Klaveren Heer</td><td>Klaveren Heer</td><td>Klaveren Heer</td><td>Klaveren Heer</td><td>Klaveren Heer</td></tr>
+			<tr><td>Klaveren Heer</td><td>Klaveren Heer</td><td>Klaveren Heer</td><td>Klaveren Heer</td><td>Klaveren Heer</td></tr>
+			<tr><td>Klaveren Heer</td><td>Klaveren Heer</td><td>Klaveren Heer</td><td>Klaveren Heer</td><td>Klaveren Heer</td></tr>
+		    </table>
+		</div>
+	    </div>
+	    <div class="row">
+		<div class="one-half column">
+		    Kaart
+		</div>
+		<div class="one-half column">
+		    Trek een kaart
+		</div>
+	    </div>
+	    <div class="row">
+		<div class="one-half column">
+		    <table>
+			<caption id="player2_name">Rob</caption>
+			<tr><td>Klaveren Heer</td><td>Klaveren Heer</td><td>Klaveren Heer</td><td>Klaveren Heer</td><td>Klaveren Heer</td></tr>
+			<tr><td>Klaveren Heer</td><td>Klaveren Heer</td><td>Klaveren Heer</td><td>Klaveren Heer</td><td>Klaveren Heer</td></tr>
+			<tr><td>Klaveren Heer</td><td>Klaveren Heer</td><td>Klaveren Heer</td><td>Klaveren Heer</td><td>Klaveren Heer</td></tr>
+		    </table>
+		</div>
+		<div class="one-half column">
+		    <table>
+			<caption id="player3_name">Rob</caption>
+			<tr><td>Klaveren Heer</td><td>Klaveren Heer</td><td>Klaveren Heer</td><td>Klaveren Heer</td><td>Klaveren Heer</td></tr>
+			<tr><td>Klaveren Heer</td><td>Klaveren Heer</td><td>Klaveren Heer</td><td>Klaveren Heer</td><td>Klaveren Heer</td></tr>
+			<tr><td>Klaveren Heer</td><td>Klaveren Heer</td><td>Klaveren Heer</td><td>Klaveren Heer</td><td>Klaveren Heer</td></tr>
+		    </table>
+		</div>
 	    </div>
 	</div>
+
 	<script src="js/pesten.js"></script> 
     </body>
 </html>
