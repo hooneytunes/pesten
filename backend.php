2ccc4ddacd80115c74d521ea9472ba7f84068075
hooneytunes <rob@familiedehoon.nl>
2018-09-04 20:01:18 +0200

    Spel gemaakt

--- backend.php
+++ backend.php
@@ -17,23 +17,29 @@
 		<div class="eight columns">&nbsp;</div>
 	    </div>
 	    <div class="row">
-	    <div class="eight columns">
-		<?php
-		/*
-		 * Laat het bestand zien als het bestaat
-		 * en anders geef de melding dat er nog geen logbestand is
-		 */
-		$filename = dirname(__FILE__) . "/log.txt";
-		if (file_exists($filename)) {
-		    $fileHandle = fopen($filename, "r");
-		    $antwoord = fread($fileHandle, filesize($filename));
-		} else	{
-		    $antwoord="Logbestand bestaat nog niet\n";
-		}
-		echo $antwoord;
-		?>
-	    </div>
-	    <div class="three columns">&nbsp;</div>
+		<div class="eight columns">
+		    <?php
+		    /*
+		     * Laat het bestand zien als het bestaat
+		     * en anders geef de melding dat er nog geen logbestand is
+		     */
+		    $filename = dirname(__FILE__) . "/log.txt";
+		    if (file_exists($filename)) {
+			$log = file($filename);
+			$sort = sort($log);
+			for ($i = 0; $i < count($log); $i++) {
+			    $regel = explode("@", $log[$i]);
+			    echo date("d/m/Y H:i:s", floor($regel[0] / 1000));
+			    echo ": " . $regel[1];
+			}
+//			$fileHandle = fopen($filename, "r");
+//			$antwoord = fread($fileHandle, filesize($filename));
+		    } else {
+			echo("Logbestand bestaat nog niet\n");
+		    }
+		    ?>
+		</div>
+		<div class="three columns">&nbsp;</div>
 	    </div>
 	</div>
     </body>
--- loggen.php
+++ loggen.php
@@ -6,11 +6,10 @@
  */
 $antwoord = "Niets";
 $tekst = filter_input(INPUT_GET, "iTekst", FILTER_SANITIZE_MAGIC_QUOTES);
+$tijd = filter_input(INPUT_GET, "iTijd", FILTER_SANITIZE_MAGIC_QUOTES);
+$antwoord = $tijd."@".$tekst . "</br>\n";
+$fileHandle = fopen(dirname(__FILE__) . "/log.txt", "a+");
+fwrite($fileHandle, $antwoord);
 
-if ($tekst !== "") {
-    $antwoord = date("H:i:s") .  "&nbsp;"  . $tekst."</br>\n";
-    $fileHandle = fopen(dirname(__FILE__)."/log.txt", "a+");
-    fwrite($fileHandle, $antwoord);
-}
 
 echo $antwoord;
